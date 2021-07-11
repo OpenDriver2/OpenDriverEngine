86ae8b103056339baed697d36d98b47dd6de6ca4
Ilya Shurumov <ishurumov@gmail.com>
2021-07-11 17:15:30 +0600

    - more Vector math exposition to lua

--- game/shared/lua_init.cpp
+++ game/shared/lua_init.cpp
@@ -28,8 +28,26 @@
 #include "math/Matrix.h"
 #include "math/psx_math_types.h"
 #include "math/isin.h"
+#include "math/squareroot0.h"
+#include "math/ratan2.h"
+#include "math/convert.h"
 
-//typedef normalize<Vector2D> vec2_normalize;
+#define VEC_OPERATORS(vec_type) \
+	/* vec - vec */\
+	sol::meta_function::addition, sol::resolve<vec_type(const vec_type&, const vec_type&)>(&operator+),\
+	sol::meta_function::subtraction, sol::resolve<vec_type(const vec_type&, const vec_type&)>(&operator-),\
+	sol::meta_function::multiplication, sol::resolve<vec_type(const vec_type&, const vec_type&)>(&operator*),\
+	sol::meta_function::division, sol::resolve<vec_type(const vec_type&, const vec_type&)>(&operator/),\
+	/* negate */\
+	sol::meta_function::unary_minus, sol::resolve<vec_type(const vec_type&)>(&operator-),
+
+// NOT USED - conflicting (SAD)
+#define VEC_FLOAT_OPERATORS(vec_type) \
+	/* vec - float */\
+	sol::meta_function::addition, sol::resolve<vec_type(const vec_type&, const float&)>(&operator+),\
+	sol::meta_function::subtraction, sol::resolve<vec_type(const vec_type&, const float&)>(&operator-),\
+	sol::meta_function::multiplication, sol::resolve<vec_type(const vec_type&, const float&)>(&operator*),\
+	sol::meta_function::division, sol::resolve<vec_type(const vec_type&, const float&)>(&operator/),\
 
 void LuaInit(sol::state& lua)
 {
@@ -45,32 +63,81 @@ void LuaInit(sol::state& lua)
 	// 3D MATH
 	auto& vec = lua["vec"].get_or_create<sol::table>();
 
+	//
+	// Vector 2D
+	//
 	vec.new_usertype<Vector2D>("vec2",
+		sol::meta_function::construct, sol::constructors<Vector2D(float), Vector2D(float, float)>(),
+		// init from table
+		sol::meta_function::construct, sol::factories(
+			[](const sol::table& table) {
+				return Vector2D(table["x"], table["y"]);
+			}),
+		VEC_OPERATORS(Vector2D)
 		"x", &Vector2D::x,
 		"y", &Vector2D::y);
 
+	//
+	// Vector 3D
+	//
 	vec.new_usertype<Vector3D>("vec3",
+		sol::call_constructor, sol::factories(
+			[](const sol::table& table) {
+				return Vector3D(table["x"], table["y"], table["z"]);
+			}),
+		sol::call_constructor, sol::constructors<Vector3D(const float&, const float&, const float&), Vector3D(const float&)>(),
+		VEC_OPERATORS(Vector3D)
+		// members
 		"x", &Vector3D::x,
 		"y", &Vector3D::y,
 		"z", &Vector3D::z);
 
+	//
+	// Vector 4D
+	//
 	vec.new_usertype<Vector4D>("vec4",
+		sol::call_constructor, sol::factories(
+			[](const sol::table& table) {
+				return Vector4D(table["x"], table["y"], table["z"], table["w"]);
+			}),
+		sol::call_constructor, sol::constructors<Vector3D(const float&), Vector4D(const float&, const float&, const float&, const float&)>(),
+		VEC_OPERATORS(Vector4D)
+		// members
 		"x", &Vector4D::x,
 		"y", &Vector4D::y,
 		"z", &Vector4D::z,
 		"w", &Vector4D::w);
 
-	vec.new_usertype<VECTOR_NOPAD>("FIXED_VECTOR",
+	//
+	// Vector 2D
+	//
+	vec.new_usertype<VECTOR_NOPAD>("FVECTOR",
+		sol::call_constructor, sol::factories(
+			[](const sol::table& table) {
+				return VECTOR_NOPAD{ table["x"], table["y"], table["z"] };
+			},
+			[]() { return VECTOR_NOPAD{ 0 }; }),
 		"vx", &VECTOR_NOPAD::vx,
 		"vy", &VECTOR_NOPAD::vy,
 		"vz", &VECTOR_NOPAD::vz);
+
+	auto& fix = lua["fix"].get_or_create<sol::table>();
+
+	vec["ONE"] = ONE;
+
+	fix["ToFixed"]		= [](const float& a)	{ return int(a * ONE_F); };
+	fix["FromFixed"]	= [](const int& a)		{ return float(a) / ONE_F; };
+
+	fix["ToFixedVector"] = &ToFixedVector;
+	fix["FromFixedVector"] = &FromFixedVector;
 
 	// extend Lua math
 	auto& gte = lua["gte"].get_or_create<sol::table>();
 
 	gte["isin"] = &isin;
 	gte["icos"] = &icos;
-	// gte["SquareRoot0"] = &SquareRoot0;
+	gte["SquareRoot0"] = &SquareRoot0;
+	gte["ratan2"] = &ratan2;
 	// gte["MulMatrix0"] = &MulMatrix0;
 
 	//-----------------------------------
