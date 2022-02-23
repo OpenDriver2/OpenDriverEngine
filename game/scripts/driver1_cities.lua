268201e492dad1a284fa3e0884afd595d65913e1
InspirationByte <ishurumov@gmail.com>
2022-02-23 11:47:24 +0300

    - ensure the correct order of D1 cities - added overlay map settings to D1 - added Flat to D1 levels list

--- game/scripts/driver1_cities.lua
+++ game/scripts/driver1_cities.lua
@@ -4,91 +4,6 @@
 -----------------------------------------------------
 
 return {
-	["Interview"] = {
-		number 		= 4,		-- city index; For necessary hardcoded stuff
-		gameId 		= 1,		-- Game Index.
-
-		skyPath		= "DRIVER2/DATA/SKY2.RAW",
-		
-		brightnessScale = 2,
-		forceNight	= true,
-
-		-- lev path for all city types
-		levPath		= "DRIVER/LEVELS/IVIEW.LEV",
-		
-		events 		= {},
-		cosmetics 	= "scripts/driver1_cosmetics.lua",
-		lcf			= "DRIVER/COSMETICS.LCF",
-		
-		-- this is still not decided
-		frontendPath = {},
-
-		-- map settings
-		overlayMap = {
-			x_offset = 197, y_offset = 318,
-			width = 384, height = 672,
-			
-			toptile = 252, dummyImage = 0x99,
-			
-			scale = 2145,	-- / 4096
-		}
-	},
-	["Training"] = {
-		number 		= 5,		-- city index; For necessary hardcoded stuff
-		gameId 		= 1,		-- Game Index.
-
-		skyPath		= "DRIVER2/DATA/SKY2.RAW",
-		
-		brightnessScale = 0.8,
-
-		-- lev path for all city types
-		levPath		= "DRIVER/LEVELS/TRAIN.LEV",
-		
-		events 		= {},
-		cosmetics 	= "scripts/driver1_cosmetics.lua",
-		lcf			= "DRIVER/COSMETICS.LCF",
-		
-		-- this is still not decided
-		frontendPath = {},
-
-		-- map settings
-		overlayMap = {
-			x_offset = 197, y_offset = 318,
-			width = 384, height = 672,
-			
-			toptile = 252, dummyImage = 0x99,
-			
-			scale = 2145,	-- / 4096
-		}
-	},
-	["NewCastle"] = {
-		number 		= 7,		-- city index; For necessary hardcoded stuff
-		gameId 		= 1,		-- Game Index.
-
-		skyPath		= "DRIVER2/DATA/SKY2.RAW",
-		
-		brightnessScale = 0.8,
-
-		-- lev path for all city types
-		levPath		= "DRIVER/LEVELS/CREDITS.LEV",
-		
-		events 		= {},
-		cosmetics 	= "scripts/driver1_cosmetics.lua",
-		lcf			= "DRIVER/COSMETICS.LCF",
-		
-		-- this is still not decided
-		frontendPath = {},
-
-		-- map settings
-		overlayMap = {
-			x_offset = 197, y_offset = 318,
-			width = 384, height = 672,
-			
-			toptile = 252, dummyImage = 0x99,
-			
-			scale = 2145,	-- / 4096
-		}
-	},
 	["Miami"] = {
 		number 		= 0,		-- city index; For necessary hardcoded stuff
 		gameId 		= 1,		-- Game Index.
@@ -109,12 +24,12 @@ return {
 
 		-- map settings
 		overlayMap = {
-			x_offset = 197, y_offset = 318,
-			width = 384, height = 672,
+			x_offset = 8, y_offset = 80,
+			width = 384, height = 256,
 			
-			toptile = 252, dummyImage = 0x99,
+			toptile = 96, dummyImage = 0,
 			
-			scale = 2145,	-- / 4096
+			scale = 2812,	-- / 4096
 		}
 	},
 	["SanFrancisco"] = {
@@ -137,16 +52,45 @@ return {
 
 		-- map settings
 		overlayMap = {
-			x_offset = 197, y_offset = 318,
-			width = 384, height = 672,
+			x_offset = 83, y_offset = 162,
+			width = 384, height = 416,
 			
-			toptile = 252, dummyImage = 0x99,
+			toptile = 156, dummyImage = 0,
 			
-			scale = 2145,	-- / 4096
+			scale = 2812,	-- / 4096
+		}
+	},
+	["Interview"] = {
+		number 		= 2,		-- city index; For necessary hardcoded stuff
+		gameId 		= 1,		-- Game Index.
+
+		skyPath		= "DRIVER2/DATA/SKY2.RAW",
+		
+		brightnessScale = 2,
+		forceNight	= true,
+
+		-- lev path for all city types
+		levPath		= "DRIVER/LEVELS/IVIEW.LEV",
+		
+		events 		= {},
+		cosmetics 	= "scripts/driver1_cosmetics.lua",
+		lcf			= "DRIVER/COSMETICS.LCF",
+		
+		-- this is still not decided
+		frontendPath = {},
+
+		-- map settings
+		overlayMap = {
+			x_offset = 0, y_offset = 0,
+			width = 448, height = 352,
+			
+			toptile = 0, dummyImage = 0,
+			
+			scale = 0,	-- / 4096
 		}
 	},
 	["LosAngeles"] = {
-		number 		= 2,		-- city index; For necessary hardcoded stuff
+		number 		= 3,		-- city index; For necessary hardcoded stuff
 		gameId 		= 1,		-- Game Index.
 
 		skyPath		= "DRIVER2/DATA/SKY2.RAW",
@@ -166,13 +110,34 @@ return {
 
 		-- map settings
 		overlayMap = {
-			x_offset = 197, y_offset = 318,
-			width = 384, height = 672,
+			x_offset = 324, y_offset = 175,
+			width = 192, height = 416,
 			
-			toptile = 252, dummyImage = 0x99,
+			toptile = 78, dummyImage = 0,
 			
-			scale = 2145,	-- / 4096
-		}
+			scale = 2812,	-- / 4096
+		},
+	},
+	["Flat"] = {
+		number 		= 4,
+		gameId		= 1,
+
+		skyPath		= "DRIVER2/DATA/SKY2.RAW",
+		
+		brightnessScale = 0.8,
+
+		-- lev path for all city types
+		levPath		= "DRIVER/LEVELS/FLAT.LEV",
+		
+		events 		= {},
+		cosmetics 	= "scripts/driver1_cosmetics.lua",
+		lcf			= "DRIVER/COSMETICS.LCF",
+		
+		-- this is still not decided
+		frontendPath = {},
+
+		-- map settings
+		overlayMap = {}
 	},
 	["NewYork"] = {
 		number 		= 5,		-- city index; For necessary hardcoded stuff
@@ -194,12 +159,54 @@ return {
 
 		-- map settings
 		overlayMap = {
-			x_offset = 197, y_offset = 318,
-			width = 384, height = 672,
-			
-			toptile = 252, dummyImage = 0x99,
-			
-			scale = 2145,	-- / 4096
-		}
+			x_offset = 126, y_offset = 93,
+			width = 384, height = 224,
+	
+			toptile = 84, dummyImage = 0,
+	
+			scale = 2812,	-- / 4096
+		},
+	},
+	["Training"] = {
+		number 		= 6,		-- city index; For necessary hardcoded stuff
+		gameId 		= 1,		-- Game Index.
+
+		skyPath		= "DRIVER2/DATA/SKY2.RAW",
+		
+		brightnessScale = 0.8,
+
+		-- lev path for all city types
+		levPath		= "DRIVER/LEVELS/TRAIN.LEV",
+		
+		events 		= {},
+		cosmetics 	= "scripts/driver1_cosmetics.lua",
+		lcf			= "DRIVER/COSMETICS.LCF",
+		
+		-- this is still not decided
+		frontendPath = {},
+
+		-- map settings
+		overlayMap = {}
+	},
+	["NewCastle"] = {
+		number 		= 7,		-- city index; For necessary hardcoded stuff
+		gameId 		= 1,		-- Game Index.
+
+		skyPath		= "DRIVER2/DATA/SKY2.RAW",
+		
+		brightnessScale = 0.8,
+
+		-- lev path for all city types
+		levPath		= "DRIVER/LEVELS/CREDITS.LEV",
+		
+		events 		= {},
+		cosmetics 	= "scripts/driver1_cosmetics.lua",
+		lcf			= "DRIVER/COSMETICS.LCF",
+		
+		-- this is still not decided
+		frontendPath = {},
+
+		-- map settings
+		overlayMap = {}
 	},
 }
