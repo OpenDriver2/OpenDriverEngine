b8946f26374fcdef6be7ba430d0ccb314380de77
Ilya Shurumov <ishurumov@gmail.com>
2022-03-09 14:18:10 +0300

    - more documentation stuff

--- game/scripts/documentation.lua
+++ game/scripts/documentation.lua
@@ -26,8 +26,8 @@ end
 
 ShowDocumentation = false
 local DocPropType = {
-	[0] = "func",
-	[1] = "prop",
+	[0] = "function",
+	[1] = "member",
 	[2] = "enum"
 }
 
@@ -40,14 +40,18 @@ function DisplayDocumentationWindow()
 	ImGui.SetNextWindowSize(500, 400, ImGuiCond.FirstUseEver)
 	if ImGui.Begin("Lua documentation") then
 		for namespace in orderedPairs(docs) do
-			local tbl = docs[namespace]
+			local docTbl = docs[namespace]
+			local tbl = {}
+			if namespace ~= "_G" then
+				tbl = _G[namespace]
+			end
 			if ImGui.CollapsingHeader(namespace) then
 			
-				for typeName in orderedPairs(tbl) do
-					local typeTbl = tbl[typeName]
+				for typeName in orderedPairs(docTbl) do
+					local typedocTbl = docTbl[typeName]
 					local typeStr = ""
-					if typeTbl.description ~= nil and string.len(typeTbl.description) > 0 then
-						typeStr = typeName .. " - " .. typeTbl.description
+					if typedocTbl.description ~= nil and string.len(typedocTbl.description) > 0 then
+						typeStr = typeName .. " - " .. typedocTbl.description
 					else
 						typeStr = typeName
 					end
@@ -60,18 +64,18 @@ function DisplayDocumentationWindow()
 							ImGui.TableSetupColumn("Description", 0, 1.25)
 							ImGui.TableHeadersRow()
 						
-							for name in orderedPairs(typeTbl.members) do
-								local propTbl = typeTbl.members[name]
+							for name in orderedPairs(typedocTbl.members) do
+								local propdocTbl = typedocTbl.members[name]
 								ImGui.TableNextRow();
 
 								ImGui.TableSetColumnIndex(0)
-								ImGui.Text(DocPropType[propTbl.type])
+								ImGui.Text(DocPropType[propdocTbl.type])
 								
 								ImGui.TableSetColumnIndex(1)
 								ImGui.Text(name)
 
 								ImGui.TableSetColumnIndex(2)
-								ImGui.Text(propTbl.desc)
+								ImGui.Text(propdocTbl.desc)
 							end
 							ImGui.EndTable()
 						end
@@ -79,6 +83,28 @@ function DisplayDocumentationWindow()
 						ImGui.TreePop()
 						ImGui.Separator()
 					end
+				end
+				-- constants table
+				if ImGui.BeginTable("constTable", 3) then
+
+					ImGui.TableSetupColumn("Type", 0, 0.35)
+					ImGui.TableSetupColumn("Name", 0, 0.7)
+					ImGui.TableHeadersRow()
+				
+					for name in orderedPairs(tbl) do
+						local prop = tbl[name]
+						
+						if type(prop) ~= "table" then
+							ImGui.TableNextRow();
+
+							ImGui.TableSetColumnIndex(0)
+							ImGui.Text(type(prop))
+							
+							ImGui.TableSetColumnIndex(1)
+							ImGui.Text(name)
+						end
+					end
+					ImGui.EndTable()
 				end
 			end
 		end
